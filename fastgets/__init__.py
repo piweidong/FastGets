df9e389c1dfe15ba6c995aadeb57496ad590fec1
ShuJuHeiKe <tonyxuourlove.com>
2018-01-06 22:53:38 +0800

    add pkg to pypi

--- fastgets/__init__.py
+++ fastgets/__init__.py
@@ -1,5 +1,7 @@
 # coding: utf8
 
+__version__ = '0.1'
+
 from .task import Task
 from .template import TemplateBase
 from .writer import CsvWriter
--- fastgets/models/__init__.py
+++ fastgets/models/__init__.py
@@ -5,6 +5,7 @@ from .. import env
 
 if env.mode == env.DISTRIBUTED:
     if env.configured:
+        # 这里不能简单实用 mock 的方式，可能会影响到用户层项目
         connect(env.MONGO_CONFIG['db'], host=env.MONGO_CONFIG['host'], port=env.MONGO_CONFIG['port'])
     else:
         raise ValueError('must call fastgets.init_fastgets_env to init env')
