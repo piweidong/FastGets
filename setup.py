6d858fd525dbf1849f2a6f51908a07d7c7f55e98
ShuJuHeiKe <tonyxuourlove.com>
2018-01-09 23:45:44 +0800

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FastGets',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/ShuJuHeiKe/FastGets',
     description='Python Crawling Framework for Humans',
     author='ShuJuHeiKe',
